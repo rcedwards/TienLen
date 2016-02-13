da602afcdbf4969597a85911c6ea62930e25c687
Robert Edwards <robert@bignerdranch.com>
2016-02-13 16:11:31 -0500

    Add tests for card

---
+++ TienLenTests/CardTests.swift
@@ -0,0 +1,26 @@
+//
+//  CardTests.swift
+//  TienLen
+//
+//  Created by Robert Edwards on 2/13/16.
+//  Copyright © 2016 Panko. All rights reserved.
+//
+
+import XCTest
+
+@testable import TienLen
+
+class CardTests: XCTestCase {
+
+    let aceOfSpades = Card(rank: .Ace, suit: .Spade)
+    let aceOfHearts = Card(rank: .Ace, suit: .Heart)
+    let aceOfHearts2 = Card(rank: .Ace, suit: .Heart)
+
+    func testEquality() {
+        XCTAssertNotEqual(aceOfSpades, aceOfHearts)
+        XCTAssertEqual(aceOfHearts, aceOfHearts2)
+
+        XCTAssertNotEqual(aceOfSpades.hashValue, aceOfHearts.hashValue)
+        XCTAssertEqual(aceOfHearts.hashValue, aceOfHearts2.hashValue)
+    }
+}
