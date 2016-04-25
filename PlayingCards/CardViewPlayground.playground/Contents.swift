183b815eb4373d6cd1a81b2f57158136df8c5725
Robert Edwards <rcedwards@gmail.com>
2016-04-24 20:52:04 -0400

    Adjust font size based on frame size

--- PlayingCards/CardViewPlayground.playground/Contents.swift
+++ PlayingCards/CardViewPlayground.playground/Contents.swift
@@ -7,6 +7,6 @@ import PlayingCards
 
 
 let card = CardView(frame: CGRect(x: 0, y: 0, width: 144, height: 200))
-card.configure(.Ace, suit: .Heart)
+card.configure(.King, suit: .Heart)
 
 XCPlaygroundPage.currentPage.liveView = card
